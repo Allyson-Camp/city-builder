f25d23fa0c3d1159bb35edbfb96b944271c59690
Allyson Camp <Allysoncamp92@gmail.com>
2023-01-13 09:58:01 -0800

    grabbed all DOM elements and set the states

--- app.js
+++ app.js
@@ -1,12 +1,25 @@
 /* Imports */
 
 /* Get DOM Elements */
+const skylineDropdown = document.getElementById('skyline-dropdown');
+const neonSkyline = document.getElementById('neon-skyline');
+const waterfrontSkyline = document.getElementById('waterfront-skyline');
+const livelySkyline = document.getElementById('lively-skyline');
+
+const artDropdown = document.getElementById('art-dropdown');
+const colorfulArt = document.getElementById('colorful-art');
+const simpleArt = document.getElementById('simple-art');
+const banksyArt = document.getElementById('banksy-art');
+
+const parkDropdown = document.getElementById('park-dropdown');
+const bridgePark = document.getElementById('bridge-park');
+const kidsPark = document.getElementById('kids-park');
+const treesPark = document.getElementById('trees-park');
 
 
 
 
 /* State */
-
 
 
 
